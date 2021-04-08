3a26e7f2a5d19049c073c3296ef5934e256874c9
MrEvil <evil.glurak@gmail.com>
2021-04-08 14:07:54 +0200

    - rename the data module import to authdata to open up the namespace data - add error handling for receiveBannedWordList - rework sub-event function to be compatible with the hub-server api later on - the info command FUCKING WORKS NOW (well, it prints the stream info to console, but better then before) - also added a clear command

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import threading
 import types
 
 # import the data.py file storing all the tokens and other neccessary info
-import data
+import data as authData
 
 def cooldown(function, duration = int(30)) -> types.FunctionType:
     """
@@ -33,21 +33,26 @@ def cooldown(function, duration = int(30)) -> types.FunctionType:
     return wrapper
 
 def receiveBannedWordList():
-    request = requests.get("http://localhost:5000/api/alpha/get/bannedWords")
+    try:
+        request = requests.get("http://localhost:5000/api/alpha/get/bannedWords")
+    except requests.exceptions.ConnectionError:
+        return set()
+    if request.status_code != 200:
+        return set()
     data = request.json()
     returner = set(data)
     return returner
 
 class Bot(commands.Bot):
 
-    bannedWords = set
+    bannedWords = set()
 
     def __init__(self):
-        super().__init__(irc_token=data.OAUTH_TOKEN,
-            client_id=data.CLIENT_ID,
-            nick=data.BOT_NICK,
-            prefix=data.BOT_PREFIX,
-            initial_channels=data.CHANNELS)
+        super().__init__(irc_token=authData.OAUTH_TOKEN,
+            client_id=authData.CLIENT_ID,
+            nick=authData.BOT_NICK,
+            prefix=authData.BOT_PREFIX,
+            initial_channels=authData.CHANNELS)
         self.bannedWords = receiveBannedWordList()
 
     async def event_ready(self):
@@ -59,8 +64,23 @@ class Bot(commands.Bot):
         await self.handle_commands(message)
 
     async def event_usernotice_subscription(self, data):
+        apiData = {}
+        if data.sub_plan == "Prime":
+            apiData["tier"] = 4
+        elif data.sub_plan == "1000":
+            apiData["tier"] = 1
+        elif data.sub_plan == "2000":
+            apiData["tier"] = 2
+        elif data.sub_plan == "3000":
+            apiData["tier"] = 3
+        if data.streak_months != None:
+            apiData["streak"] = data.streak_months
+        else:
+            apiData["streak"] = 0
         if data.streak_months != 0:
-            await data.channel.send(f"{data.user.name} has extended their sub-streak by one month. It's now at {data.streak_months} months!")
+            await data.channel.send(f'{data.user.name} has extended their sub-streak on tier {apiData["tier"]} by one month. It\'s now at {data.streak_months} months!')
+        else:
+            await data.channel.send(f'{data.user.name} has just subscribed on tier {apiData["tier"]}')
 
     @commands.command(name="test")
     @cooldown
@@ -70,13 +90,8 @@ class Bot(commands.Bot):
     @commands.command(name="info")
     @cooldown
     async def info(self, context):
-        await context.send("Following authentication problems this command is currently unavailable")
-        #channel = bot.get_channel(data.CHANNELS[0])
-        #if channel is not None:
-        #    print(channel)
-        #    stream = await channel.get_stream()
-        #    for key, value in stream:
-        #        await ctx.send(f'{key} : {value}')
+        stream = authData.getStreamInfo()
+        print(stream)
 
     @commands.command(name="so")
     @cooldown
@@ -100,7 +115,11 @@ class Bot(commands.Bot):
     @commands.command(name="TownOfSalem")
     @cooldown
     async def townOfSalem(self, context):
-        await context.send("Add me on Town of Salem: MrEvilInSalem")        
+        await context.send("Add me on Town of Salem: MrEvilInSalem")
+
+    @commands.command(name="clear")
+    async def clear(self, context):
+        await context.channel.clear()
 
 def check_user(name):
     # Example from twitch docs on how to get user info using userid
