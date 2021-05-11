adbb6005da875d763fae8775a8341dd97efa45e2
MrEvil <evil.glurak@gmail.com>
2021-05-11 16:27:08 +0200

    - no need for json, remove the import - import all twitchio errors and dataclasses - import inspect to make shure certain arguments are what they are supposed to be - import messages - remove typedef of cooldown(function)   b/c idk the type of the passed function   it's supposed to be used as a decorator anyways - remove and re-add receiveBannedWordList while also adding a template for an api class - api class: collection of interactions with api server. Not working yet - add async implemention of dict iterator - add messages to send messages manually (not shure if it works yet) - add async method to send repeating messages (not shure if it works yet) - some more formating - add a repeated message to be sent - re-enable cooldown - rename info to streamdata since that's what it does - shoutout now targets rep if no argument is passed - add help command: If not on my channel it links to mine. Well, to a (not yet existing) panel of mine - uptime now does something. Not quite there yet, but it does something - most info commands are now specific to my channel - clear now sends an error message if .clear is not allowed for the bot

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
 from twitchio.ext import commands
+from twitchio.errors import *
+from twitchio import dataclasses
 import time
-import json
 import asyncio
 import requests
 import threading
 import types
+import inspect
 
 # import the data.py file storing all the tokens and other neccessary info
-import data as authData
+import data as generalData
 
-def cooldown(function, duration = int(30)) -> types.FunctionType:
+import const_messages as messages
+
+def cooldown(function, duration=int(30)):
     """
     Cooldown decorator for command functions
 
     Optional argument: duration = integer
 
-    How it works: Launches a timing thread utilizing sleep when the command is called.
-    And until that thread finishes the internal on_cooldown variable will prevent another call of the function
+    How it works
+        Launches a timing thread utilizing sleep when the command is called.
+        And until that thread finishes the internal on_cooldown variable
+        will prevent another call of the function
     """
     function.on_cooldown = False
+
     def sleeper():
         function.on_cooldown = True
         time.sleep(duration)
         function.on_cooldown = False
+
     async def wrapper(*args, **kwargs):
         if function.on_cooldown:
             print(f"Function {function.__name__} on cooldown")
@@ -32,16 +40,6 @@ def cooldown(function, duration = int(30)) -> types.FunctionType:
             timer.start()
     return wrapper
 
-def receiveBannedWordList():
-    try:
-        request = requests.get("http://localhost:5000/api/alpha/get/bannedWords")
-    except requests.exceptions.ConnectionError:
-        return set()
-    if request.status_code != 200:
-        return set()
-    data = request.json()
-    returner = set(data)
-    return returner
 
 def iterateThroughDict(dictionary, funtionToExecute):
     for key in dictionary:
@@ -50,7 +48,10 @@ def iterateThroughDict(dictionary, funtionToExecute):
         else:
             funtionToExecute((key, dictionary[key]))
 
-async def asyncIterateThroughDict(dictionary, asyncFunction, *, functionIsAsync=True):
+
+async def asyncIterateThroughDict(dictionary,
+                                  asyncFunction,
+                                  functionIsAsync=True):
     for key in dictionary:
         if type(dictionary[key]) is dict:
             await asyncIterateThroughDict(dictionary[key], asyncFunction, functionIsAsync=functionIsAsync)
@@ -59,20 +60,89 @@ async def asyncIterateThroughDict(dictionary, asyncFunction, *, functionIsAsync=
                 await asyncFunction((key, dictionary[key]))
             else:
                 asyncFunction((key, dictionary))
+
+# Could be useful:
+#
+# add_listener(func, name: str = None)
+# Method which adds a coroutine as an extra listener.
+# This can be used to add extra event listeners to the bot.
+# Parameters:
+#     func (coro [Required]) – The coroutine to assign as a listener.
+#     name (str [Required]) – The event to register. E.g “event_message”.
+#
+# Add async func to async loop
+#
+# chan = bot.get_channel("channelname")
+# loop = asyncio.get_event_loop()
+# loop.create_task(chan.send("Send this message"))
+
+def sendMessageToChannel(channel: str, message: str):
+    for i in generalData.CHANNELS:
+        if i.lower() == channel.lower():
+            raise ValueError
+    loop = asyncio.get_event_loop()
+    loop.create_task(Bot.get_channel(channel).send(message))
+
+async def sendRepeatedMessage(channel=Bot.get_channel("MrEvilOnTwitch"),
+                        message: str, delay=120,
+                        loop=asyncio.get_event_loop()):
+    await asyncio.sleep(delay)
+    channel.send(message)
+    loop.create_task(sendRepeatedMessage(channel, message, delay, loop))
+
+
+class api:
+    """
+    Wrapper class for interacting with the localy hosted web-API
+    """
+
+    apiURL = "http://localhost:5000/api/alpha/"
+
+    def interact(service, action="get", data=None):
+        if action is not "get" or "set":
+            raise TypeError(message="action must be get or set")
+        try:
+            r = requests.get(apiURL+service, headers=data)
+        except requests.exceptions.ConnectionError:
+            return None
+        return r
+
+    def receiveBannedWordList(self):
+        request = self.interact("bannedWords")
+        if request is None:
+            return
+        if request.status_code != 200:
+            return
+        data = request.json()
+        returner = tuple(data)
+        return returner
+
+
+def receiveBannedWordList():
+    request = requests.get(api.apiURL+"bannedWords")
+    if request is None:
+        return
+    if request.status_code != 200:
+        return
+    data = request.json()
+    returner = tuple(data)
+    return returner
+
 class Bot(commands.Bot):
 
-    bannedWords = set()
+    bannedWords = tuple
 
     def __init__(self):
-        super().__init__(irc_token=authData.OAUTH_TOKEN,
-            client_id=authData.CLIENT_ID,
-            nick=authData.BOT_NICK,
-            prefix=authData.BOT_PREFIX,
-            initial_channels=authData.CHANNELS)
+        super().__init__(irc_token=generalData.OAUTH_TOKEN,
+                         client_id=generalData.CLIENT_ID,
+                         nick=generalData.BOT_NICK,
+                         prefix=generalData.BOT_PREFIX,
+                         initial_channels=generalData.CHANNELS)
         self.bannedWords = receiveBannedWordList()
 
     async def event_ready(self):
         print(f'Bot ready | {self.nick}')
+        await sendRepeatedMessage("MrEvilOnTwitch", messages.commands, delay=300)
 
     async def event_message(self, message):
         # remove messages with blacklisted words first, then execute the command if it isn't removed
@@ -89,7 +159,7 @@ class Bot(commands.Bot):
             apiData["tier"] = 2
         elif data.sub_plan == "3000":
             apiData["tier"] = 3
-        if data.streak_months != None:
+        if data.streak_months is not None:
             apiData["streak"] = data.streak_months
         else:
             apiData["streak"] = 0
@@ -99,64 +169,80 @@ class Bot(commands.Bot):
             await data.channel.send(f'{data.user.name} has just subscribed on tier {apiData["tier"]}')
 
     @commands.command(name="test")
-    #@cooldown
+    @cooldown
     async def test(self, context):
         await context.send(f'Hello {context.author.name}')
 
-    @commands.command(name="info")
-    #@cooldown
-    async def info(self, context):
+    @commands.command(name="streamdata")
+    @cooldown
+    async def streamdata(self, context):
         """
         Grabs the publicly available information of the stream chat is has been called from and prints it to console output
         """
-        #stream = authData.getStreamInfo()
-        #print(stream)
-        #print("----------------------------")
         print(context.channel.name)
-        await asyncIterateThroughDict(authData.getStreamInfo(context.channel.name.lower()), print, functionIsAsync=False)
+        await asyncIterateThroughDict(generalData.getStreamInfo(context.channel.name.lower()), print, functionIsAsync=False)
         await context.send("worked")
 
     @commands.command(name="so")
-    #@cooldown
+    @cooldown
     async def shoutout(self, context):
-        await context.send(f"{context.content}")
         message = context.content
         if len(message) > 4:
-            target = message[4:]
-            print(target)
-            await context.send(f"Shoutout to {target}, who you might be able to find at https://twitch.tv/{target}!")
-            await context.send("Also, blame the helix api for no last game checking")
+            target = message[len(generalData.BOT_PREFIX)+3:]
+        else:
+            target = "irepptar"
+        stream = generalData.getStreamInfo(target)
+        if stream != generalData.offlineData:
+            string = f'{target} is currently live playing {stream["data"][0]["game_name"]}. Check them out here: twitch.tv/{target}'
+            await context.send(string)
+        else:
+            await context.send(f"{target} is currently offline, but you might find them here: twitch.tv/{target}")
+
+    @commands.command(name="help")
+    @cooldown
+    async def help(self, context):
+        if context.channel.name == "mrevilontwitch":
+            await context.send("This bot is selfmade and open-source. The commands can be found in the info tabs")
+        else:
+            await context.send("This bot has been made by twitch.tv/MrEvilOnTwitch. The commands are available in his info tabs")
 
     @commands.command(name="uptime")
-    #@cooldown
+    @cooldown
     async def uptime(self, context):
         streamName = context.channel.name
-        streamInfo = authData.getStreamInfo(streamName.lower())
+        streamInfo = generalData.getStreamInfo(streamName.lower())
         offlineData = {'data': [], 'pagination': {}}
         if streamInfo == offlineData:
-            context.send('OI, the stream is offline, there is no uptime to be found')
+            await context.send('OI, the stream is offline, there is no uptime to be found')
         else:
-            context.send(f'The stream started at {streamInfo["data"][0]["started_at"]}')
+            await context.send(f'The stream started at {streamInfo["data"][0]["started_at"]}')
 
-    @commands.command(name="Discord")
-    #@cooldown
+    @commands.command(name="discord")
+    @cooldown
     async def discord(self, context):
-        await context.send("It seems that you are interested in our Discord server. You can join over here: discord.gg/3a4ZseU")
+        if context.channel.name == "mrevilontwitch":
+            await context.send("It seems that you are interested in our Discord server. You can join over here: discord.gg/3a4ZseU")
 
-    @commands.command(name="DestinyJoin")
-    #@cooldown
+    @commands.command(name="destiny")
+    @cooldown
     async def destinyJoin(self, context):
-        await context.send("If you want to join me with your guardian (and my fireteam settings are on public), you can use this joincode: 76561198078422715")
+        if context.channel.name == "mrevilontwitch":
+            await context.send("If you want to join me with your guardian (and my fireteam settings are on public), you can use this joincode: 76561198078422715")
 
-    @commands.command(name="TownOfSalem")
-    #@cooldown
+    @commands.command(name="town")
+    @cooldown
     async def townOfSalem(self, context):
-        await context.send("Add me on Town of Salem: MrEvilInSalem")
+        if context.channel.name == "mrevilontwitch":
+            await context.send("Add me on Town of Salem: MrEvilInSalem")
 
     @commands.command(name="clear")
     async def clear(self, context):
         if context.user.is_mod:
-            await context.channel.clear()
+            try:
+                await context.channel.clear()
+            except TwitchIOBException:
+                await context.send("The bot is not a mod on this channel. Unable to use the clear command")
+
 
 if __name__ == "__main__":
     bot = Bot()
