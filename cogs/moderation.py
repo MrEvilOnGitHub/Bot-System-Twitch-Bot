a49e633b0963ef551c6b561587fb5ee9a68a94d5
MrEvil <evil.glurak@gmail.com>
2021-07-14 21:19:47 +0200

    mod cog can now autoremove messages containing banned words

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -6,12 +6,19 @@ class mod:
     def __init__(self, bot):
         self.bot = bot
 
+    bannedWords = ("söojdafjiosdfjioj") # only for testing rn
+
     async def event_message(self, context: dataclasses.Context):
-        pass
+        if context.author.name == self.bot.nick:
+            # don't react to own messages
+            return
+        if any(x in context.content for x in self.bannedWords):
+            print(f"Timing out {context.author.name} b/c of of a banned word")
+            await context.channel.timeout(context.author.name, duration=1,reason="Banned word detected")
 
     @commands.command(name="clear")
     async def clear(self, context: dataclasses.Context):
-        if context.user.is_mod:
+        if context.author.is_mod:
             try:
                 await context.channel.clear()
             except TwitchIOBException:
